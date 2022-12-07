49aa18c655aa24b7cf0ebddb05d449e6a4432594
Misha Dzhemula <medvedmisha23@gmail.com>
2022-12-07 18:12:38 +0200

    Improvment

--- src/index.js
+++ src/index.js
@@ -10,37 +10,45 @@ function sendAnErr(res, err) {
   res.end(String(err));
 }
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    const index = fs.createReadStream('./src/index.html');
+function handleIndex(req, res) {
+  const index = fs.createReadStream('./src/index.html');
 
-    pipeline(index, res, err => {
-      if (err) {
-        sendAnErr(res, err);
+  pipeline(index, res, err => {
+    if (err) {
+      sendAnErr(res, err);
+    }
+  });
+}
+
+function handleUpload(req, res) {
+  const form = formidable();
+
+  form.parse(req, (err, fields) => {
+    if (err) {
+      sendAnErr(res, err);
+
+      return;
+    }
+
+    fs.writeFileSync('./form.json', JSON.stringify(fields, null, 2));
+
+    const readJson = fs.createReadStream('./form.json');
+
+    pipeline(readJson, res, error => {
+      if (error) {
+        sendAnErr(res, error);
       }
     });
+  });
+}
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    handleIndex(req, res);
   }
 
   if (req.url === '/upload' && req.method === 'POST') {
-    const form = formidable();
-
-    form.parse(req, (err, fields) => {
-      if (err) {
-        sendAnErr(res, err);
-
-        return;
-      }
-
-      fs.writeFileSync('./form.json', JSON.stringify(fields, null, 2));
-
-      const readJson = fs.createReadStream('./form.json');
-
-      pipeline(readJson, res, error => {
-        if (error) {
-          sendAnErr(res, error);
-        }
-      });
-    });
+    handleUpload(req, res);
   }
 });
 
