e40d099832dfcaf5151842501a8e5db107ef9001
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2023-03-12 12:07:34 +0200

    Form data (with Node.js) - completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const http = require('http');
+const fs = require('fs');
+
+const { fromDataParser } = require('./modules/formDataParser');
+const { updateJSON } = require('./modules/updateJSON');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/expenses') {
+    let body = '';
+
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on('end', () => {
+      const dataFromForm = fromDataParser(body);
+
+      try {
+        const updatedJSON = updateJSON(dataFromForm);
+
+        res.end(updatedJSON);
+      } catch (error) {
+        res.statusCode = 404;
+        res.end();
+      }
+    });
+  } else {
+    try {
+      const expensePage = fs.readFileSync('src/public/index.html');
+
+      res.end(expensePage);
+    } catch (error) {
+      res.statusCode = 404;
+      res.end();
+    }
+  }
+});
+
+server.listen(5700);
