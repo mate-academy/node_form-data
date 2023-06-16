983d49fab7ab8c22b95f5c94dcbd2c9eaffa4d10
ivanm2706 <ivanmorhun664@gmail.com>
2023-06-16 14:36:19 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,1 +1,90 @@
 'use strict';
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    const pathFileHTML = path.join(__dirname, 'pages', 'index.html');
+
+    const readStream = fs.createReadStream(pathFileHTML);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+
+    readStream.pipe(res);
+
+    readStream.on('error', (err) => {
+      res.statusCode = 404;
+      res.end('Error: ' + err);
+    });
+
+    return;
+  }
+
+  if (req.url === '/data') {
+    const multiparty = require('multiparty');
+
+    const form = new multiparty.Form();
+
+    form.parse(req, (err, fields) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end('Error', err);
+
+        return;
+      }
+
+      const newInfo = {};
+
+      for (const key in fields) {
+        newInfo[key] = fields[key][0];
+      }
+
+      const pathToJSON = path.join(__dirname, 'data', 'data.json');
+
+      fs.readFile(pathToJSON, 'utf8', (errorRead, jsonData) => {
+        if (errorRead) {
+          res.statusCode = 404;
+          res.end('Can not read file', errorRead);
+
+          return;
+        }
+
+        let dataFromJson;
+
+        try {
+          dataFromJson = JSON.parse(jsonData);
+        } catch (e) {
+          dataFromJson = [];
+        }
+
+        const newData = JSON.stringify([...dataFromJson, newInfo], null, 2);
+
+        fs.writeFile(pathToJSON, newData, (errWrite) => {
+          if (errWrite) {
+            res.statusCode = 404;
+            res.end('Can not write file', errWrite);
+
+            return;
+          }
+
+          const htmlResponse = `<pre>${newData}</pre>`;
+
+          res.setHeader('Content-Type', 'text/html');
+          res.end(htmlResponse);
+        });
+      });
+    });
+
+    return;
+  }
+
+  res.statusCode = 404;
+  res.end('Not Found');
+});
+
+server.listen(3000, () => {
+  process.stdout.write('Server is running on http://localhost:3000');
+});
