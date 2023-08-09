8a691a308cd5a12ad6a189f33e4b883dfb061b52
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-09 12:04:16 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,72 +6,87 @@ const path = require('path');
 
 const server = new http.Server();
 
+const PATHS = {
+  index: 'GET /',
+  convert: 'POST /convert',
+};
+
+// left here for simplicity(may be moved into .html if needed)
+const BASE_HTML = `
+  <h1>Expence</h1>
+
+  <form action="/convert" method="post" enctype="application/json">
+    <label for="date">Date:</label>
+    <input type="date" name="date" id="date" required /><br />
+
+    <label for="text">Title:</label>
+    <input type="text" name="title" id="text" required /><br />
+
+    <label for="number">Amount:</label>
+    <input type="number" name="amount" id="number" required />
+
+    <input type="submit" />
+  </form>
+`;
+
 server.on('request', (req, res) => {
-  if (req.url === '/' && req.method === 'GET') {
-    res.writeHeader(200, { 'Content-Type': 'text/html' });
+  switch (req.method + ' ' + req.url) {
+    case PATHS.index:
+      res.writeHeader(200, { 'Content-Type': 'text/html' });
 
-    res.end(`
-      <h1>Expence</h1>
+      res.end(BASE_HTML);
+      break;
 
-      <form action="/convert" method="post" enctype="application/json">
-        <label for="date">Date:</label>
-        <input type="date" name="date" id="date" required /><br />
+    case PATHS.convert:
+      const chunks = [];
 
-        <label for="text">Title:</label>
-        <input type="text" name="title" id="text" required /><br />
-
-        <label for="number">Amount:</label>
-        <input type="number" name="amount" id="number" required />
-
-        <input type="submit" />
-      </form>
-    `);
-  } else if (req.url === '/convert' && req.method === 'POST') {
-    const chunks = [];
-
-    req.on('data', (chunk) => {
-      chunks.push(chunk);
-    });
-
-    req.on('end', () => {
-      const dataString = Buffer.concat(chunks).toString().split('&');
-      const data = {};
-
-      dataString.forEach((item) => {
-        const [key, value] = item.split('=');
-
-        data[key] = value;
+      req.on('data', (chunk) => {
+        chunks.push(chunk);
       });
 
-      const JSONdata = JSON.stringify(data, null, 2);
+      req.on('end', () => {
+        const dataString = Buffer.concat(chunks).toString().split('&');
+        const data = {};
 
-      res.writeHeader(200, {
-        'Content-Type': 'text/html',
+        dataString.forEach((item) => {
+          const [key, value] = item.split('=');
+
+          data[key] = value;
+        });
+
+        const JSONdata = JSON.stringify(data, null, 2);
+
+        res.writeHeader(200, {
+          'Content-Type': 'text/html',
+        });
+
+        fs.writeFile(path.join(__dirname, 'data.json'), JSONdata, (err) => {
+          if (err) {
+            res.writeHeader(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal server error');
+          }
+        });
+
+        const formatedHtmlJson = `
+          <h1>Expence</h1>
+
+          <p>
+            ${JSONdata.replace(/\n/g, '<br />')}
+          </p>
+        `;
+
+        res.end(formatedHtmlJson);
       });
+      break;
 
-      fs.writeFile(path.join(__dirname, 'data.json'), JSONdata, (err) => {
-        if (err) {
-          res.writeHeader(500, { 'Content-Type': 'text/plain' });
-          res.end('Internal server error');
-        }
-      });
-
-      const formatedHtmlJson = `
-        <h1>Expence</h1>
-
-        <p>
-          ${JSONdata.replace(/\n/g, '<br />')}
-        </p>
-      `;
-
-      res.end(formatedHtmlJson);
-    });
-  } else {
-    res.writeHeader(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
+    default:
+      res.writeHeader(404, { 'Content-Type': 'text/plain' });
+      res.end('Not found');
   }
 });
 
-server.on('error', () => {});
+// eslint-disable-next-line no-console
+server.on('error', () => console.log('Server error'));
 
-server.listen(3000);
+// eslint-disable-next-line no-console
+server.listen(3000, () => console.log('Server started on port 3000'));
