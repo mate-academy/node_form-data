2b72ad59571bc33d2c826b87e56fea10ce2805d6
Ilia <koleshnik160799@gmail.com>
2023-08-25 10:17:15 +0300

    fix code

--- src/index.js
+++ src/index.js
@@ -11,71 +11,61 @@ const PORT = 5700;
 const server = http.Server();
 
 server.on('request', (req, res) => {
-  if(req.method === 'POST') {
+  if (req.method === 'POST') {
     const jsonPath = path.resolve('src', 'index.json');
     const chunks = [];
 
     if (!fs.existsSync(jsonPath)) {
-      res.readStatus = 500;
-      res.end()
+      res.statusCode = 500;
+      res.end();
+
       return;
     }
+
     const jsonFile = fs.readFileSync(jsonPath, 'utf8');
-
-    if (!fs.existsSync(jsonFile)) {
-      res.readStatus = 500;
-      res.end()
-      return;
-    }
     const jsonParse = JSON.parse(jsonFile);
-    
-    if (req.url.slice(1) === 'favicon.ico') {
-      return;
-    }
 
-    req.on('data', (chunk) => {    
+    req.on('data', (chunk) => {
       chunks.push(chunk
         .toString()
         .split('&')
         .map(info => info.split('=')));
     });
-    
+
     req.on('end', () => {
       jsonParse.push(Object.fromEntries(...chunks));
-      
       fs.writeFileSync(jsonPath, JSON.stringify(jsonParse, null, 2));
-    })
+    });
 
     res.writeHead(302, { Location: '/' });
-
     res.end();
   } else if (req.method === 'GET') {
     const htmlPath = path.resolve('public', 'index.html');
     const gzip = zlib.createGzip();
 
     if (!fs.existsSync(htmlPath)) {
-      res.readStatus = 500;
+      res.statusCode = 500;
       res.end('Error from server');
+
       return;
     }
 
-    const htmlFile =  fs.createReadStream(htmlPath);
+    const htmlFile = fs.createReadStream(htmlPath);
 
     res.setHeader('Content-Encoding', 'gzip');
 
     pipeline(htmlFile, gzip, res, (err) => {
-      if(err) {
-        res.readStatus = 500;
+      if (err) {
+        res.statusCode = 500;
         res.end();
       }
     });
   } else {
-    res.readStatus = 404;
+    res.statusCode = 404;
     res.end('Page not found.');
-    return;
   }
 });
 
 server.listen(PORT, () => {
   process.stdout.write(`Server is working http://localhost:${PORT}\n`);
-})
+});
