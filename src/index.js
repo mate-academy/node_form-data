87f0287bd935182da779cc70c27f8cf0e77f230f
grinjk1993 <grinjk1993@gmail.com>
2023-09-15 20:19:15 +0300

    form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const express = require('express');
+const { readFileSync } = require('fs');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.end(readFileSync('src/index.html'));
+});
+
+app.post('/format', (req, res) => {
+  res.end(JSON.stringify(req.body)
+    .replace(/["{}]/g, '')
+    .replace(/,/g, '\n')
+    .replace(/:/g, ': ')
+  );
+});
+
+app.listen(3000);
