dd3cc10142693aff938544618bbb2eb99b1591c0
Boiko Vlad <bojko.vladislav16@gmail.com>
2023-10-29 17:11:35 +0100

    Linter has been fixed

--- src/index.js
+++ src/index.js
@@ -1,49 +1,49 @@
-"use strict";
+'use strict';
 
-const { Server } = require("http");
-const fs = require("fs");
-const zlib = require("zlib");
-const path = require("path");
+const { Server } = require('http');
+const fs = require('fs');
+const zlib = require('zlib');
+const path = require('path');
 
 const server = new Server();
 
-server.on("request", (req, res) => {
-  if (req.method === "POST") {
+server.on('request', (req, res) => {
+  if (req.method === 'POST') {
     const chunks = [];
 
-    req.on("data", (chunk) => {
+    req.on('data', (chunk) => {
       chunks.push(chunk);
     });
 
-    req.on("end", () => {
-      const body = Buffer.concat(chunks).toString().split("&");
+    req.on('end', () => {
+      const body = Buffer.concat(chunks).toString().split('&');
       const formData = {};
 
       body.forEach((chunk) => {
-        const [key, value] = chunk.split("=");
+        const [key, value] = chunk.split('=');
 
         formData[key] = value;
       });
 
-      res.setHeader("Content-Type", "application/json");
+      res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify(formData));
     });
 
     return;
   }
 
-  const fileStream = fs.createReadStream(path.join(__dirname, "/index.html"));
+  const fileStream = fs.createReadStream(path.join(__dirname, '/index.html'));
   const gzip = zlib.createGzip();
 
-  res.setHeader("Content-Encoding", "gzip");
+  res.setHeader('Content-Encoding', 'gzip');
 
   fileStream
-    .on("error", () => new Error("Error reading a file!"))
+    .on('error', () => new Error('Error reading a file!'))
     .pipe(gzip)
-    .on("error", () => new Error("Error compressing a file!"))
+    .on('error', () => new Error('Error compressing a file!'))
     .pipe(res);
 
-  res.on("close", () => {
+  res.on('close', () => {
     fileStream.destroy();
     gzip.destroy();
   });
