290431d1c34f07b93716f58ecf67a9b7336c4bb6
Mykhailo <eglet777@ukr.net>
2024-02-03 23:34:06 +0200

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,50 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return new http.Server((req, res) => {
+    if (req.url === '/' && req.method === 'GET') {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(fs.readFileSync('public/index.html'));
+    } else if (req.url === '/add-expense' && req.method === 'POST') {
+      let data = '';
+
+      req.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      req.on('end', () => {
+        const params = new URLSearchParams(data);
+
+        if (!params.get('date')
+          || !params.get('title')
+          || !params.get('amount')) {
+          fs.writeFileSync('db/expense.json', JSON.stringify({}));
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('Invalid form data');
+
+          return;
+        }
+
+        const obj = {
+          date: params.get('date'),
+          title: params.get('title'),
+          amount: params.get('amount'),
+        };
+
+        const str = JSON.stringify(obj);
+
+        fs.writeFileSync('db/expense.json', str);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(str);
+      });
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Page not found');
+    }
+  });
 }
 
 module.exports = {
