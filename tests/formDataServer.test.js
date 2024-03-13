bfc46d40e5417f8cfde8148b0b47edd6ffef9392
Misha Grinko <glukym@gmail.com>
2024-03-13 17:00:36 +0200

    fix node14 deps

--- tests/formDataServer.test.js
+++ tests/formDataServer.test.js
@@ -5,7 +5,10 @@ const { createServer } = require('../src/createServer.js');
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
-const { Server } = require('http');
+const { Server, Agent } = require('http');
+
+// this prevents `socket hang up` for Node.js 20.10+
+axios.defaults.httpAgent = new Agent({ keepAlive: false });
 
 const PORT = 5701;
 const HOST = `http://localhost:${PORT}`;
@@ -14,13 +17,11 @@ describe('Form Data Server', () => {
   describe('createServer', () => {
     describe('basic scenarios', () => {
       it('should create a server', () => {
-        expect(createServer)
-          .toBeInstanceOf(Function);
+        expect(createServer).toBeInstanceOf(Function);
       });
 
-      it('should create an instance of Server', async() => {
-        expect(createServer())
-          .toBeInstanceOf(Server);
+      it('should create an instance of Server', async () => {
+        expect(createServer()).toBeInstanceOf(Server);
       });
     });
 
@@ -38,7 +39,7 @@ describe('Form Data Server', () => {
         server.close();
       });
 
-      it('should save data for valid expense on "POST /submit-expense" request', async() => {
+      it('should save data for valid expense on "POST /submit-expense" request', async () => {
         fs.writeFileSync(dataPath, JSON.stringify({}));
 
         const expense = {
@@ -55,7 +56,7 @@ describe('Form Data Server', () => {
         expect(savedData).toStrictEqual(expense);
       });
 
-      it('should reject request without all params on "POST /submit-expense" request', async() => {
+      it('should reject request without all params on "POST /submit-expense" request', async () => {
         fs.writeFileSync(dataPath, JSON.stringify({}));
 
         const expense = {
@@ -70,13 +71,11 @@ describe('Form Data Server', () => {
         } catch (err) {
           expect(err.response.data.length).toBeGreaterThan(0);
 
-          expect(
-            JSON.parse(fs.readFileSync(dataPath))
-          ).toStrictEqual({});
+          expect(JSON.parse(fs.readFileSync(dataPath))).toStrictEqual({});
         }
       });
 
-      it('should return JSON on "POST /submit-expense" request', async() => {
+      it('should return JSON on "POST /submit-expense" request', async () => {
         const expense = {
           date: '2024-01-25',
           title: 'Test Expense',
@@ -88,7 +87,7 @@ describe('Form Data Server', () => {
         expect(response.data).toStrictEqual(expense);
       });
 
-      it('should return 404 for invalid url', async() => {
+      it('should return 404 for invalid url', async () => {
         expect.assertions(1);
 
         try {
