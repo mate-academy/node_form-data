cd13cbfa8f6aaee0cf8ff219749385f630ebecdf
Basil-Panasiuk <v.panasiuk.job@gmil.com>
2024-04-27 20:13:59 +0300

    Feat: implement server

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,79 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+const { pipeline } = require('stream');
+const formidable = require('formidable');
+const path = require('path');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = new http.Server();
+
+  server.on('request', async (req, res) => {
+    const url = new URL(req.url, `http:${req.headers.host}`);
+
+    res.setHeader('Content-Type', 'text/plaint');
+
+    if (url.pathname === '/') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+
+      const homeFile = fs.createReadStream('public/index.html');
+
+      pipeline(homeFile, res, (err) => {
+        res.statusCode = 500;
+
+        return res.end(String(err));
+      });
+    } else if (url.pathname !== '/add-expense') {
+      res.statusCode = 404;
+
+      return res.end('Not found');
+    } else {
+      const form = new formidable.IncomingForm();
+
+      try {
+        const [fields] = await form.parse(req);
+
+        const { date, title, amount } = fields;
+
+        if (!date || !title || !amount) {
+          res.statusCode = 400;
+
+          return res.end('Invalid form');
+        }
+
+        const fileContent = JSON.stringify({
+          date,
+          title,
+          amount,
+        });
+
+        const filePath = path.resolve(__dirname, '../db/expense.json');
+        const fileStream = fs.createWriteStream(filePath);
+
+        fileStream.write(fileContent, (err) => {
+          if (err) {
+            res.statusCode = 500;
+            res.end(String(err));
+          }
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.end(fileContent);
+        });
+
+        fileStream.end();
+      } catch (err) {
+        res.statusCode = err?.httpCode || 400;
+
+        return res.end(String(err));
+      }
+    }
+  });
+
+  server.on('error', () => {});
+
+  return server;
 }
 
 module.exports = {
