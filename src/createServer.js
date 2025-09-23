790db020a25c9c9cbbd76f3496aeb3501a1613d5
Oleh Mysko <olegmysko1996@gmail.com>
2025-09-23 18:54:23 +0300

    se

--- src/createServer.js
+++ src/createServer.js
@@ -8,19 +8,19 @@ function createServer() {
   return http.createServer((req, res) => {
     const indexPath = path.resolve('src', 'index.html');
 
-   
     if (req.method === 'GET' && req.url === '/') {
       try {
         const file = fs.readFileSync(indexPath);
+
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(file);
       } catch {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('file not found');
       }
+
       return;
     }
-
 
     if (req.method === 'POST' && req.url === '/add-expense') {
       const chunks = [];
@@ -32,30 +32,30 @@ function createServer() {
           const buffer = Buffer.concat(chunks).toString();
           const obj = JSON.parse(buffer);
 
-
           if (!obj.date || !obj.title || !obj.amount) {
             res.writeHead(400, { 'Content-Type': 'text/plain' });
             res.end('Invalid expense data');
+
             return;
           }
 
           let arr = [];
 
-
           try {
             const file = fs.readFileSync('db/expense.json', 'utf-8');
+
             arr = JSON.parse(file);
-            if (!Array.isArray(arr)) arr = [];
+
+            if (!Array.isArray(arr)) {
+              arr = [];
+            }
           } catch {
             arr = [];
           }
 
-
           arr.push(obj);
 
-
-          fs.writeFileSync('db/expense.json', JSON.stringify(arr, null, 2));
-
+          fs.writeFileSync('db/expense.json', JSON.stringify(obj, null, 2));
 
           res.writeHead(200, { 'Content-Type': 'application/json' });
           res.end(JSON.stringify(obj));
@@ -67,7 +67,6 @@ function createServer() {
 
       return;
     }
-
 
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not found');
