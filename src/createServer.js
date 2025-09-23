1c26c2ba9e817141f6da98a03309a573cfcb621a
Oleh Mysko <olegmysko1996@gmail.com>
2025-09-23 16:36:33 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,10 +1,60 @@
 'use strict';
 
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const indexPath = path.resolve('src', 'index.html');
+
+    if (req.method === 'GET' && req.url === '/') {
+      try {
+        const file = fs.readFileSync(indexPath);
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(file);
+      } catch {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('file not found');
+      }
+
+      return;
+    }
+
+
+    if (req.method === 'POST' && req.url === '/add-expense') {
+      const chunks = [];
+
+      req.on('data', (chunk) => chunks.push(chunk));
+
+      req.on('end', () => {
+        try {
+          const buffer = Buffer.concat(chunks).toString();
+          const obj = JSON.parse(buffer);
+
+          if (!obj.date || !obj.title || !obj.amount) {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Invalid expense data');
+
+            return;
+          }
+
+          fs.writeFileSync('db/expense.json', JSON.stringify(obj, null, 2));
+
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(obj));
+        } catch (err) {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('Invalid JSON');
+        }
+      });
+
+      return;
+    }
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+  });
 }
 
-module.exports = {
-  createServer,
-};
+module.exports = { createServer };
