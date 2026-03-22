cd2cbd7b731712e91989299ad6449135b0021790
Nazarii-Lesniak <naziksay@gmail.com>
2026-03-22 16:40:21 +0200

    changed

--- src/createServer.js
+++ src/createServer.js
@@ -6,7 +6,22 @@ const path = require('path');
 
 function createServer() {
   return http.createServer((request, response) => {
-    if (request.url === '/add-expense' && request.method === 'POST') {
+    if (request.url === '/' && request.method === 'GET') {
+      const htmlPath = path.resolve(__dirname, 'index.html');
+
+      fs.readFile(htmlPath, 'utf-8', (error, data) => {
+        if (error) {
+          response.statusCode = 500;
+          response.end('Internal Server Error');
+
+          return;
+        }
+
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/html');
+        response.end(data);
+      });
+    } else if (request.url === '/add-expense' && request.method === 'POST') {
       const chunks = [];
 
       request.on('data', (chunk) => {
@@ -15,7 +30,18 @@ function createServer() {
 
       request.on('end', () => {
         try {
-          const parsedData = JSON.parse(Buffer.concat(chunks).toString());
+          const bodyStr = Buffer.concat(chunks).toString();
+          let parsedData;
+
+          const contentType = request.headers['content-type'] || '';
+
+          if (contentType === 'application/json') {
+            parsedData = JSON.parse(bodyStr);
+          } else {
+            const params = new URLSearchParams(bodyStr);
+
+            parsedData = Object.fromEntries(params);
+          }
 
           if (!parsedData.amount || !parsedData.title || !parsedData.date) {
             response.statusCode = 400;
@@ -24,15 +50,32 @@ function createServer() {
             return;
           }
 
-          const db = path.join(__dirname, '../db/expense.json');
-          const stringifiedData = JSON.stringify(parsedData);
+          const dbPath = path.join(__dirname, '../db/expense.json');
 
-          fs.writeFileSync(db, stringifiedData);
+          fs.writeFile(dbPath, JSON.stringify(parsedData, null, 2), (error) => {
+            if (error) {
+              response.statusCode = 500;
+              response.end('Internal Server Error');
 
-          response.statusCode = 200;
-          response.setHeader('Content-Type', 'application/json');
-          response.end(JSON.stringify(parsedData));
-        } catch {
+              return;
+            }
+
+            const accept = request.headers.accept || '';
+
+            if (accept.includes('text/html')) {
+              response.statusCode = 200;
+              response.setHeader('Content-Type', 'text/html');
+
+              response.end(`
+                <pre>${JSON.stringify(parsedData, null, 2)}</pre>
+              `);
+            } else {
+              response.statusCode = 200;
+              response.setHeader('Content-Type', 'application/json');
+              response.end(JSON.stringify(parsedData));
+            }
+          });
+        } catch (error) {
           response.statusCode = 400;
           response.end('Invalid data');
         }
