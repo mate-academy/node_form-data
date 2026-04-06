4b2b09cb9db157b3f8be91fe1c5bed2962eec2b9
Volodymyr Kolosov <kolosovvova0@gmail.com>
2026-04-06 13:03:29 +0100

    form data

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,65 @@
 'use strict';
 
+const http = require('node:http');
+const fs = require('node:fs');
+const path = require('node:path');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = new http.Server();
+
+  server.on('request', (req, res) => {
+    const baseUrl = req.url;
+    const dbPath = path.resolve('db', 'expense.json');
+
+    if (baseUrl === '/add-expense') {
+      if (req.method !== 'POST') {
+        res.statusCode = 400;
+
+        return res.end();
+      }
+
+      const chunks = [];
+
+      req.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+
+      req.on('end', () => {
+        try {
+          const buffer = Buffer.concat(chunks).toString();
+          const data = JSON.parse(buffer);
+
+          if (!data.date || !data.title || !data.amount) {
+            fs.writeFileSync(dbPath, JSON.stringify({}));
+
+            res.statusCode = 400;
+            res.setHeader('Content-Type', 'application/json');
+
+            return res.end(JSON.stringify('Missing fields'));
+          }
+
+          fs.writeFileSync(dbPath, JSON.stringify(data));
+
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+
+          return res.end(JSON.stringify(data));
+        } catch (e) {
+          res.statusCode = 400;
+          res.setHeader('Content-Type', 'application/json');
+
+          return res.end(JSON.stringify({ error: 'Invalid JSON' }));
+        }
+      });
+
+      return;
+    }
+
+    res.statusCode = 404;
+    res.end();
+  });
+
+  return server;
 }
 
 module.exports = {
